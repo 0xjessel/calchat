1b46e501dda0705709d9b898eb109a4715db5aba
Nelson Hoang <nelson.hoang@gmail.com>
2012-03-23 17:40:51 -0700

    geolocation works but have not stored the values

---
+++ public/js/geo.js
@@ -0,0 +1,64 @@
+
+initiate_geolocation();
+
+function initiate_geolocation() {
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors);
+    }
+    else
+    {
+        yqlgeo.get('visitor', normalize_yql_response);
+    }
+}
+
+function handle_errors(error)
+{
+    switch(error.code)
+    {
+        case error.PERMISSION_DENIED: alert("user did not share geolocation data");
+        break;
+
+        case error.POSITION_UNAVAILABLE: alert("could not detect current position");
+        break;
+
+        case error.TIMEOUT: alert("retrieving position timedout");
+        break;
+
+        default: alert("unknown error");
+        break;
+    }
+}
+
+function normalize_yql_response(response)
+{
+    if (response.error)
+    {
+        var error = { code : 0 };
+        handle_error(error);
+        return;
+    }
+
+    var position = {
+        coords :
+        {
+            latitude: response.place.centroid.latitude,
+            longitude: response.place.centroid.longitude
+        },
+        address :
+        {
+            city: response.place.locality2.content,
+            region: response.place.admin1.content,
+            country: response.place.country.content
+        }
+    };
+
+    handle_geolocation_query(position);
+}
+
+function handle_geolocation_query(position)
+{
+alert('Lat: ' + position.coords.latitude +  
+                ' Lon: ' + position.coords.longitude);
+}
+
