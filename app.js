135436d463e16346d5b9ed649d17b599a62bb8b2
Mark Wei <markwei@gmail.com>
2012-03-26 17:45:44 -0700

    skeleton for nearest buildings, issue #19

--- app.js
+++ app.js
@@ -183,6 +183,12 @@ io.sockets.on('connection', function (socket) {
 			}
 		});
 	});
+    
+    socket.on('get nearest buidings', function(location) {
+        // get buildings from redis db
+        var buildings = null;
+        sockets.emit('nearest buildings', buildings);
+    });
 });
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
