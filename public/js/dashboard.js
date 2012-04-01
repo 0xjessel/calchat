a9c5d03bc7d351ebf941a5536716dde7b0077931
Jesse Chen <github@jessechen.net>
2012-03-31 23:48:45 -0700

    fix #45.  modified alert

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 	if (rooms.length == 1) {
 		var alert = $('.alert');
 		alert.removeClass('hidden');
-		alert.html('<a class="close" data-dismiss="alert">x</a>No rooms!');
+		alert.html('<a class="close" data-dismiss="alert">x</a>You haven\'t added any chatrooms yet!  Add a new chatroom in the navbar above or in the Add Chatroom section below.');
 	}
 });
