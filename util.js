506cbb6bd46583d50ce2bb6a2ce88067c9809d08
Mark Wei <markwei@gmail.com>
2012-04-08 21:04:05 -0700

    comment util.js

--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+// Utility functions shared by all server code
+
 var TwilioClient = require('twilio').Client
 var client = new TwilioClient('ACdd4df176cb5b41e6a424f60633982d8e', '8c2cc16d9a8570469569682b92283030', 'http://calchat.net:3000');
 var phone = client.getPhoneNumber('+15107468123');
@@ -11,14 +13,19 @@ client1.select(1);
 var client2 = redis.createClient(null, redisUrl);
 client2.select(2);
 
+// helper function to send an SMS notification
+// to: user id to send message to
+// mid: message id that generated the notification
 function mentionSMS(to, mid) {
 	// get to's phone number
 	client2.hget('user:'+to, 'phone', function (err, reply) {
 		if (!err && !reply) {
 			var phoneNum = reply;	
+			// fetch the message object
 			client2.hmget('message:'+mid, 'from', 'to', 'text', function (err, replies) {
 				if (!err && replies.length) {
 					var fromUid = replies[0];
+					// fetch nickname of the user that generated the notification
 					client2.hget('user:'+fromUid, 'nick', function (err, reply) {
 						if (!err && !reply) {
 							var from = reply;
@@ -32,6 +39,7 @@ function mentionSMS(to, mid) {
 								txt = txt+'..';
 							}
 							msg = msg+txt;
+							// call helper function
 							sendSMS(phoneNum, msg, null, function (sms) {
 								console.log('done');
 							})
@@ -49,20 +57,26 @@ function mentionSMS(to, mid) {
 	});
 }
 
+// helper function to send an SMS notification
 function sendSMS(number, message, opts, callback) {
 	console.log('sending SMS');
 	console.log('number: '+number);
 	console.log('message: '+message);
+	// uses twilio library
 	phone.sendSms(number, message, opts, callback);
 }
 
+// helper function to send multiple SMS notifications
 function sendMultipleSMS(numbers, message, opts, callback) {
 	for (var i = 0; i < numbers.length; i++) {
 		sendSMS(numbers[i], message, opts, callback);
 	}
 }
 
-// Returns an object containing the id, official name, pretty name, and title
+// helper function to turn a room id into a room object
+// rawId: the room id
+// callerId: id of the client who calls the function
+// callback: passes a room object containing the id, official name, pretty name, title, etc
 function getRoomInfo(rawId, callerId, callback) {
 	debug('getRoomInfo', rawId);
 	// check if the room is a class
@@ -187,16 +201,22 @@ function getRoomInfo(rawId, callerId, callback) {
 	});
 }
 
+// helper function to get multiple room objects at once
+// roomIds: list of room ids
+// callerId: user id of the client calling this function
+// callback: passes a list of room objects
 function getRoomsInfo(roomIds, callerId, callback) {
 	debug('getRoomsInfo');
 	if (!roomIds.length) {
 		callback([]);
 	}
 	
+	// deduplicate
 	var temp = {};
 	for (var i = 0; i < roomIds.length; i++) {
 		var roomId = roomIds[i];
 		var lastChar = roomId.charAt(roomId.length - 1);
+		// cleanup special characters
 		if (lastChar == '#' || lastChar == '$') {
 			roomId = roomId.substring(0, roomId.length - 1);
 		}
@@ -220,10 +240,12 @@ function getRoomsInfo(roomIds, callerId, callback) {
 				// preserve order
 				rooms[n] = room;
 
+				// when all room objects have been returned, callback
 				if (added == roomIds.length) {
 					var ret = [];
 					for (var i = 0; i < rooms.length; i++) {
 						var r = rooms[i];
+						// remove null responses
 						if (r != null) {
 							ret.push(r);
 						}
@@ -236,21 +258,23 @@ function getRoomsInfo(roomIds, callerId, callback) {
 	};
 }
 
-// prepends room to rooms (use this only if rooms exists!)
+// helper function to prepend room to rooms (use this only if rooms exists!)
 function prependRoom(room, unreads, rooms) {
 	debug('prependRoom', room, rooms);
+	// find if room exists in rooms
 	var index = -1;
 	for (var i = 0; i < rooms.length; i++) {
 		if (rooms[i] == room) {
 			index = i;
 		}
 	}
+	// if not exist, move it to the front
 	if (index == -1) {
 		rooms.unshift(room);
 		if (unreads) {
 			unreads.unshift(new Date().getTime());
 		}
-	} else {
+	} else { // if exists, splice it out
 		rooms.unshift(rooms.splice(index, 1)[0]);
 		if (unreads) {
 			unreads.unshift(unreads.splice(index, 1)[0]);
@@ -258,21 +282,24 @@ function prependRoom(room, unreads, rooms) {
 	}
 }
 
-// return true if any form of room is valid
-// return false if room cannot possibly be valid
-// if true, then you must use rawId, which contains the raw id
+// helper function to determine whether a room id is valid
+// roomId: id of the room
+// callback: passes 2 things
+// 		isValid: true if any form of roomid is valid
+//		rawId: if isValid, caller must use rawId, the normalized room id
 function isValid(roomId, callback) {
 	debug('isValid', roomId);
 	if (!roomId) {
 		callback(false);
 		return;
 	} else {
+		// sanitize input
 		roomId = stripHigh(roomId, false);
 
 		// try to find room in the database
 		var score = stringScore(roomId);
 		client0.zrangebyscore('validrooms', score, score, function(err, rooms) {
-			// room is valid if it is the raw id or an abbreviation (ELENG40 or EE40)
+			// room is valid if it is the raw id or an abbreviation (ELENG40 or EE40) or a redirect (306SODA)
 			if (!err && rooms.length) {
 				var sameLastChars = [];
 				for (var i = 0; i < rooms.length; i++) {
@@ -281,7 +308,7 @@ function isValid(roomId, callback) {
 
 					var lastChar = suggestion.charAt(suggestion.length - 1);
 					
-					// if $ is at the end then it means roomId was 306SODA and you need to redirect to current class held at 306SODA
+					// if $ is at the end then it means roomId was a room like 306SODA and you need to redirect to class held at 306SODA at the current time
 					if (lastChar == '$') {
 						getRoomInfo(suggestion.substring(0, suggestion.length-1), null, function(room) {
 							if (room) {
@@ -292,16 +319,19 @@ function isValid(roomId, callback) {
 						});
 						return;
 					} else {
+						// a # at the end signifies an abbreviated room id
 						if (lastChar == '#') {
 								// remove # at the end
 								suggestion = suggestion.substring(0, suggestion.length - 1);
 						} 
 
+						// in most cases, there will be only one suggestion which matches the room id
 						if (suggestion == roomId && rooms.length == 1) {
 							callback(true, suggestion);
 							return;
 						}
 
+						// when there are conflicts (because of stringScore and double precision), generate all suggestions
 						var sameLastChar = 0;
 						for (var j = 0; j < Math.min(suggestion.length-1, roomId.length-1); j++) {
 							var roomIdChar = roomId.charAt(roomId.length-1-j);
@@ -320,6 +350,7 @@ function isValid(roomId, callback) {
 					}
 				};
 
+				// pick best suggestion from list
 				sameLastChars.sort(function(a,b) {
 					return b.sameLastChar - a.sameLastChar;
 				});
@@ -329,6 +360,7 @@ function isValid(roomId, callback) {
 				// not in validrooms db. Check if it is a private chat to another user
 				if (roomId.indexOf(':') != -1) {
 					var uids = roomId.split(':');
+					// both users have to exist
 					client2.exists('user:'+uids[0], function(err, exists) {
 						if (!err && exists) {
 							client2.exists('user:'+uids[1], function(err, exists) {
@@ -402,7 +434,12 @@ function getLastHalfHour() {
 	return (now.getHours() + (now.getMinutes() >= 30 ? .5 : 0)).toFixed(1);
 }
 
-
+// helper function to simplify autocompletion and typeaheads
+// stringScore() takes any string as input, and returns a hash H such that H1 of a string1 that is the beginning of another string2 is always smaller than the H2 of that string2.
+// The H3 of - a string3 whose first nth character from the left greater (alphanumerically) than the nth character of a string4 - is always greater than the H4 of that string4
+// H('abc') < H('abc0')
+// H('abd') > H('abc9f9f9ff9f9f9f')
+// these properties are used in zrange db queries
 function stringScore(string, strip) {
 	if (!string) return '-inf';
 
@@ -418,6 +455,11 @@ function stringScore(string, strip) {
 	return hash;
 }
 
+// helper function to simplify autocompletion and typeaheads
+// capStringScore() returns the hash H of the string that is 'one higher' than the input string
+// a string1 is 'one higher' than a string2 when string1 == string2 except for the last characters c1 and c2, and c1 = c2+1
+// this gives the nice behavior where stringScore(S) <= stringScore(S . any_string) < capStringScore(S)
+// these properties are used in zrange db queries
 function capStringScore(string) {
 	if (!string) return '+inf';
 
